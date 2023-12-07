35dc20198afcef7a4d88bfe5d58d33f269001d03
javier barrera <fcbjavier12@hotmail.com>
2023-12-07 03:39:55 -0300

    Nuevos cambios

--- HotelApp/Client/wwwroot/js/Utilidades.js
+++ HotelApp/Client/wwwroot/js/Utilidades.js
@@ -25,69 +25,6 @@
 	});
 }
 
-window.sweetAlertInterop = {
-    showSuccessDialog: function (title, text) {
-        return new Promise((resolve) => {
-            Swal.fire({
-                title: title,
-                text: text,
-                icon: "success"
-            }).then(() => {
-                resolve(true);
-            });
-        });
-    }
-};
-
-
-// SweetAlertInterop.js
-window.showSweetAlert = function (title, showDenyButton, showCancelButton, confirmButtonText, denyButtonText) {
-    return Swal.fire({
-        title: title,
-        showDenyButton: showDenyButton,
-        showCancelButton: showCancelButton,
-        confirmButtonText: confirmButtonText,
-        denyButtonText: denyButtonText
-    }).then((result) => {
-        return result;
-    });
-};
-
-window.handleSwalResult = (result) => {
-    return new Promise((resolve) => {
-        Swal.fire(result).then((swalResult) => {
-            if (swalResult.isConfirmed) {
-                resolve(true);
-            } else if (swalResult.isDenied || swalResult.dismiss === Swal.DismissReason.cancel) {
-                resolve(false);
-            }
-        });
-    });
-};
-
-
-//function CustomSave(titulo, mensaje, tipo) {
-//    return new Promise((resolve) => {
-//        Swal.fire({
-//            title: titulo,
-//            text: mensaje,
-//            icon: tipo,
-//            showDenyButton: true,
-//            showCancelButton: true,
-//            confirmButtonText: "Guardar",
-//            denyButtonText: `No guardar`
-//        }).then((result) => {
-//            if (result.isConfirmed) {
-//                Swal.fire("Guarado", " ", "success");
-//                resolve(true); // Resuelve la promesa con true si se confirma
-//            } else if (result.isDenied) {
-//                Swal.fire("No guardado", " ", "info");
-//                resolve(false); // Resuelve la promesa con false si se deniega
-//            }
-//        });
-//    });
-//}
-
 function CustomError(titulo, mensaje, tipo) {
     return new Promise((resolve) => {
         Swal.fire({
